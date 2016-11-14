8ed5c75fea12851c03026fd57da4b4b0d3490fb5
Robert Kaye <rob@musicbrainz.org>
2016-11-14 20:18:23 +0100

    Interim check-in

--- fix-table.sh
+++ fix-table.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-OLD_DB=musicbrainz_old_db
-NEW_DB=musicbrainz_db
-TABLE=musicbrainz.artist
+OLD_DB=$1
+NEW_DB=$2
+TABLE=$3
+OUT_DIR=$4
 
 pg_dump -U musicbrainz -h musicbrainz_old_db -t $TABLE -a -f /tmp/$TABLE-old.sql $OLD_DB
 pg_dump -U musicbrainz -h musicbrainz_old_db -t $TABLE -a -f /tmp/$TABLE-new.sql $NEW_DB
 sort /tmp/$TABLE-old.sql > /tmp/$TABLE-old-sorted.sql
 sort /tmp/$TABLE-new.sql > /tmp/$TABLE-new-sorted.sql
 diff -Naur /tmp/$TABLE-old-sorted.sql /tmp/$TABLE-new-sorted.sql > /tmp/$TABLE.diff
-./table-diff $TABLE /tmp/$TABLE.diff
+./table-diff $TABLE /tmp/$TABLE.diff > $OUT_DIR/$TABLE.sql
+
+rm -f /tmp/$TABLE-old.sql /tmp/$TABLE-new.sql /tmp/$TABLE-old-sorted.sql /tmp/$TABLE-new-sorted.sql 
