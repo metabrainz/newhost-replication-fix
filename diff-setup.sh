3cb6e0a052baa699fd5bb824885c5e8cbecc0621
Michael Wiencek <mwtuea@gmail.com>
2016-11-14 17:27:48 -0600

    Remove bad host setting

--- diff-setup.sh
+++ diff-setup.sh
@@ -5,8 +5,8 @@ NEW_DB=$2
 TABLE=$3
 OUT_DIR=$4
 
-pg_dump -U musicbrainz -h musicbrainz_old_db -t $TABLE -a -f /tmp/$TABLE-old.sql $OLD_DB
-pg_dump -U musicbrainz -h musicbrainz_old_db -t $TABLE -a -f /tmp/$TABLE-new.sql $NEW_DB
+pg_dump -U musicbrainz -t $TABLE -a -f /tmp/$TABLE-old.sql $OLD_DB
+pg_dump -U musicbrainz -t $TABLE -a -f /tmp/$TABLE-new.sql $NEW_DB
 sort /tmp/$TABLE-old.sql > /tmp/$TABLE-old-sorted.sql
 sort /tmp/$TABLE-new.sql > /tmp/$TABLE-new-sorted.sql
 diff -Naur /tmp/$TABLE-old-sorted.sql /tmp/$TABLE-new-sorted.sql > /tmp/$TABLE.diff
