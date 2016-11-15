dcbde73075e8bef7410b9517c7c3cef81df39733
Michael Wiencek <mwtuea@gmail.com>
2016-11-14 18:46:11 -0600

    Use pk info to generate correct deletes

--- table-diff.py
+++ table-diff.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+import string
 import sys
 import re
 from table_info import REPLICATED_TABLES
@@ -34,9 +35,26 @@ while True:
 
     if p_minus.match(line):
         columns = line.split('\t')
-        columns[0] = columns[0][1:]
-        id = int(columns[0])
-        delete_lines.append("DELETE FROM %s WHERE id = %d;" % (table, id))
+        columns[0] = columns[0][1:]  # remove minus
+
+        conditions = []
+        for pk in REPLICATED_TABLES[table]:
+            column_name, column_position, data_type, is_nullable = pk
+            value = columns[column_position - 1]
+            if is_nullable == "YES" and value == "\\N":
+                value = "NULL"
+                conditions.append("%s IS NULL" % column_name)
+            else:
+                if data_type in ("integer", "bigint"):
+                    value = int(value)
+                elif data_type in ("character", "character varying", "date", "text", "uuid"):
+                    value = "'%s'" % string.replace(value, "'", "''")
+                else:
+                    raise ValueError("Unhandled data_type: " + data_type)
+                conditions.append("%s = %s" % (column_name, value))
+
+        conditions = " AND ".join(conditions)
+        delete_lines.append("DELETE FROM %s WHERE %s;" % (table, conditions))
         continue
 
     if p_plus.match(line):
